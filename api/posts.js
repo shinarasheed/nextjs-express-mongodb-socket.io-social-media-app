6178ab5518e2798201ea76c519d75732cdc8fa69
shina <rasheedshinaopeyemi@gmail.com>
2021-04-19 17:03:16 +0100

    implemented lots of routes for post

--- api/posts.js
+++ api/posts.js
@@ -1,308 +1,163 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const UserModel = require("../models/UserModel");
-const PostModel = require("../models/PostModel");
-const FollowerModel = require("../models/FollowerModel");
-const uuid = require("uuid").v4;
+const authMiddleware = require('../middleware/authMiddleware');
+const UserModel = require('../models/UserModel');
+const PostModel = require('../models/PostModel');
+const FollowerModel = require('../models/FollowerModel');
 
-// CREATE A POST
-
-router.post("/", authMiddleware, async (req, res) => {
+//create post
+router.post('/', authMiddleware, async (req, res) => {
   const { text, location, picUrl } = req.body;
 
   if (text.length < 1)
-    return res.status(401).send("Text must be atleast 1 character");
+    return res.status(400).send('Text must be atleast 1 character');
 
   try {
     const newPost = {
       user: req.userId,
-      text
+      text,
     };
     if (location) newPost.location = location;
     if (picUrl) newPost.picUrl = picUrl;
 
     const post = await new PostModel(newPost).save();
-
-    const postCreated = await PostModel.findById(post._id).populate("user");
-
-    return res.json(postCreated);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
-  }
-});
-
-// GET ALL POSTS
-
-router.get("/", authMiddleware, async (req, res) => {
-  const { pageNumber } = req.query;
-
-  const number = Number(pageNumber);
-  const size = 8;
-
-  try {
-    let posts;
-
-    if (number === 1) {
-      posts = await PostModel.find()
-        .limit(size)
-        .sort({ createdAt: -1 })
-        .populate("user")
-        .populate("comments.user");
-    }
-    //
-    else {
-      const skips = size * (number - 1);
-      posts = await PostModel.find()
-        .skip(skips)
-        .limit(size)
-        .sort({ createdAt: -1 })
-        .populate("user")
-        .populate("comments.user");
-    }
-
-    if (posts.length === 0) {
-      return res.json([]);
-    }
-
-    let postsToBeSent = [];
-    const { userId } = req;
-
-    const loggedUser = await FollowerModel.findOne({ user: userId });
-
-    if (loggedUser.following.length === 0) {
-      postsToBeSent = posts.filter(post => post.user._id.toString() === userId);
-    }
-    //
-    else {
-      for (let i = 0; i < loggedUser.following.length; i++) {
-        const foundPostsFromFollowing = posts.filter(
-          post =>
-            post.user._id.toString() === loggedUser.following[i].user.toString() 
-        );
-
-        if (foundPostsFromFollowing.length > 0) postsToBeSent.push(...foundPostsFromFollowing);
-      }
-      
-      const foundOwnPosts = posts.filter(post => post.user._id.toString() === userId);
-      if (foundOwnPosts.length > 0) postsToBeSent.push(...foundOwnPosts);
-      
-      
-    }
-
-     postsToBeSent.length > 0 &&
-      postsToBeSent.sort((a, b) => [new Date(b.createdAt) - new Date(a.createdAt)]);
-
-
-    return res.json(postsToBeSent);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
-  }
-});
-
-// GET POST BY ID
-
-router.get("/:postId", authMiddleware, async (req, res) => {
-  try {
-    const post = await PostModel.findById(req.params.postId)
-      .populate("user")
-      .populate("comments.user");
-
-    if (!post) {
-      return res.status(404).send("Post not found");
-    }
-
     return res.json(post);
   } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
+    console.error(error.message);
+    res.status(500).send('server error');
   }
 });
 
-// DELETE POST
-
-router.delete("/:postId", authMiddleware, async (req, res) => {
+//get all post
+router.get('/', authMiddleware, async (req, res) => {
   try {
-    const { userId } = req;
-
-    const { postId } = req.params;
-
-    const post = await PostModel.findById(postId);
-    if (!post) {
-      return res.status(404).send("post not found");
-    }
-
-    const user = await UserModel.findById(userId);
-
-    if (post.user.toString() !== userId) {
-      if (user.role === "root") {
-        await post.remove();
-        return res.status(200).send("Post deleted Successfully");
-      } else {
-        return res.status(401).send("Unauthorized");
-      }
-    }
-
-    await post.remove();
-    return res.status(200).send("Post deleted Successfully");
+    const posts = await PostModel.find({})
+      .sort({ createdAt: -1 })
+      .populate('user')
+      .populate('commets.user');
+    return res.json(posts);
   } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
+    console.error(error.message);
+    return res.status(500).send('server error');
   }
 });
 
-// LIKE A POST
+//get post by id
+router.get('/:id', authMiddleware, async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.id)
+      .populate('user')
+      .populate('comments.user');
 
-router.post("/like/:postId", authMiddleware, async (req, res) => {
+    if (!post) {
+      return res.status(404).send('Post not found');
+    }
+    return res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send('server error');
+  }
+});
+
+//delete post
+router.delete('/:postId', authMiddleware, async (req, res) => {
+  const { userId } = req;
+
+  const { postId } = req.params;
+
+  const post = await PostModel.findById(postId);
+  if (!post) {
+    return res.status(404).send('Post not found');
+  }
+
+  const user = await UserModel.findById(userId);
+
+  if (post.user.toString() !== userId) {
+    if (user.role === 'root') {
+      await post.remove();
+      return res.status(204).send('post deleted successfully');
+    } else {
+      return res.status(403).send('unauthorized');
+    }
+  }
+
+  await post.remove();
+  return res.status(204).send('post deleted successfully');
+});
+
+// like a post
+router.post('/like/:postId', authMiddleware, async (req, res) => {
   try {
     const { postId } = req.params;
     const { userId } = req;
 
     const post = await PostModel.findById(postId);
+
     if (!post) {
-      return res.status(404).send("No Post found");
+      return res.status(404).send('post not found');
     }
 
     const isLiked =
-      post.likes.filter(like => like.user.toString() === userId).length > 0;
+      post.likes.filter((like) => like.user.toString() === userId).length > 0;
 
     if (isLiked) {
-      return res.status(401).send("Post already liked");
+      return res.status(400).send('post already liked by you');
     }
 
     await post.likes.unshift({ user: userId });
     await post.save();
-
-    return res.status(200).send("Post liked");
+    return res.status(200).send('post liked');
   } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
+    console.error(error.message);
+    return res.status(500).send('server error');
   }
 });
 
-// UNLIKE A POST
-
-router.put("/unlike/:postId", authMiddleware, async (req, res) => {
+// unlike a post
+router.put('/unlike/:postId', authMiddleware, async (req, res) => {
   try {
     const { postId } = req.params;
     const { userId } = req;
 
     const post = await PostModel.findById(postId);
     if (!post) {
-      return res.status(404).send("No Post found");
+      return res.status(404).send('post not found');
     }
 
-    const isLiked =
-      post.likes.filter(like => like.user.toString() === userId).length === 0;
+    const notLiked =
+      post.likes.filter((like) => like.user.toString() === userId).length === 0;
 
-    if (isLiked) {
-      return res.status(401).send("Post not liked before");
+    if (notLiked) {
+      return res.status(400).send('post not like yet');
     }
 
-    const index = post.likes.map(like => like.user.toString()).indexOf(userId);
+    const index = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(userId);
 
     await post.likes.splice(index, 1);
-
     await post.save();
-
-    return res.status(200).send("Post Unliked");
+    return res.status(200).send('post unliked');
   } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
+    console.error(error.message);
+    return res.status(500).send('server error');
   }
 });
 
-// GET ALL LIKES OF A POST
-
-router.get("/like/:postId", authMiddleware, async (req, res) => {
+//get all likes
+router.get('/like/:postId', authMiddleware, async (req, res) => {
   try {
     const { postId } = req.params;
 
-    const post = await PostModel.findById(postId).populate("likes.user");
+    const post = await PostModel.findById(postId).populate('likes.user');
     if (!post) {
-      return res.status(404).send("No Post found");
+      return res.status(404).send('post not found');
     }
 
     return res.status(200).json(post.likes);
   } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
-  }
-});
-
-// CREATE A COMMENT
-
-router.post("/comment/:postId", authMiddleware, async (req, res) => {
-  try {
-    const { postId } = req.params;
-
-    const { text } = req.body;
-
-    if (text.length < 1)
-      return res.status(401).send("Comment should be atleast 1 character");
-
-    const post = await PostModel.findById(postId);
-
-    if (!post) return res.status(404).send("Post not found");
-
-    const newComment = {
-      _id: uuid(),
-      text,
-      user: req.userId,
-      date: Date.now()
-    };
-
-    await post.comments.unshift(newComment);
-    await post.save();
-
-    return res.status(200).json(newComment._id);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
-  }
-});
-
-// DELETE A COMMENT
-
-router.delete("/:postId/:commentId", authMiddleware, async (req, res) => {
-  try {
-    const { postId, commentId } = req.params;
-    const { userId } = req;
-
-    const post = await PostModel.findById(postId);
-    if (!post) return res.status(404).send("Post not found");
-
-    const comment = post.comments.find(comment => comment._id === commentId);
-    if (!comment) {
-      return res.status(404).send("No Comment found");
-    }
-
-    const user = await UserModel.findById(userId);
-
-    const deleteComment = async () => {
-      const indexOf = post.comments.map(comment => comment._id).indexOf(commentId);
-
-      await post.comments.splice(indexOf, 1);
-
-      await post.save();
-
-      return res.status(200).send("Deleted Successfully");
-    };
-
-    if (comment.user.toString() !== userId) {
-      if (user.role === "root") {
-        await deleteComment();
-      } else {
-        return res.status(401).send("Unauthorized");
-      }
-    }
-
-    await deleteComment();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(`Server error`);
+    console.error(error.message);
+    return res.status(500).send('server error');
   }
 });
 
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ nextApp.prepare().then(() => {
   app.use('/api/signup', require('./api/signup'));
   app.use('/api/auth', require('./api/auth'));
   app.use('/api/search', require('./api/search'));
-  // app.use("/api/posts", require("./api/posts"));
+  app.use('/api/posts', require('./api/posts'));
   // app.use("/api/profile", require("./api/profile"));
 
   app.all('*', (req, res) => handle(req, res));
