7e616addb53f437961d40d1d83d5805a467cba59
shina <rasheedshinaopeyemi@gmail.com>
2021-04-20 14:47:50 +0100

    fetched post from the backend and render on the frontend

--- components/Post/CommentInputField.js
+++ components/Post/CommentInputField.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'semantic-ui-react';
 
-const CommentInputField = () => {
-  return <div>CommentInputField</div>;
+const CommentInputField = ({ postId, user, setComments }) => {
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+  return (
+    <Form reply>
+      <Form.Input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add Comment"
+        action={{
+          color: 'blue',
+          icon: 'edit',
+          loading: loading,
+          disabled: text === '' || loading,
+        }}
+      />
+    </Form>
+  );
 };
 
 export default CommentInputField;
